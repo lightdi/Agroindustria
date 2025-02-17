f3920906e63419a14f54cced9dcd612b8aa3509a
lightdi <lightdi@hotmail.com>
2025-02-17 00:50:21 -0300

    Adicção da criar e editar analises

--- analises/controle/views.py
+++ analises/controle/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import Projeto, Amostra, Analise
-from .forms import ProjetoForm
+from .forms import ProjetoForm, AnaliseForm, AmostraForm
 
 # Create your views here.
 
@@ -9,7 +10,7 @@ from .forms import ProjetoForm
 def index(request):
     # Se o usuário for administrador, ele pode ver todos os projetos
     if request.user.administrador:
-        projetos = Projeto.objects.all()
+        projetos = Projeto.objects.all().order_by('-ativo','-data')
     else:
         # Caso contrário, busca projetos onde o usuário é o criador ou onde ele tem permissão de visualização
         projetos = Projeto.objects.filter(
@@ -17,7 +18,7 @@ def index(request):
         ) | Projeto.objects.filter(
             permissaoprojeto__usuario=request.user, 
             permissaoprojeto__pode_visualizar=True
-        )
+        ).order_by('-ativo','-data')
 
     return render(request, "index.html", {"projetos": projetos})
 
@@ -55,11 +56,121 @@ def editar_projeto(request, id):
     return render(request, 'editar_projeto.html', {'form': form, 'projeto': projeto})
 
 @login_required
+def excluir_projeto(request, id):
+    # Verifica se o usuário é administrador
+    if not request.user.administrador:
+        messages.error(request, "Você não tem permissão para excluir projetos.")
+        return redirect('index')  # Redireciona de volta para a página inicial ou outra página desejada
+
+    projeto = get_object_or_404(Projeto, id=id)
+
+    # Exclui o projeto
+    projeto.delete()
+
+    messages.success(request, "Projeto excluído com sucesso.")
+    return redirect('index')
+
+
+##Carregand os dados do projeto
+@login_required
 def projeto(request, id):
-    analises = Analise.objects.all()
-    return render(request, "projeto.html", {"analises": analises})
+    analises = Analise.objects.all().order_by('-id')
+    return render(request, "projeto.html", {"analises": analises, "projeto_id": id})
+
+
+@login_required
+def criar_analise(request, projeto_id):
+    projeto = get_object_or_404(Projeto, id=projeto_id)  # Obtém o projeto
+    if request.method == "POST":
+        form = AnaliseForm(request.POST, projeto=projeto)  # Passa o projeto
+        if form.is_valid():
+            form.save(usuario=request.user)  # Passa o usuário logado
+            return redirect('analise', id=projeto.id)  # Redireciona
+    else:
+        form = AnaliseForm(projeto=projeto)  # Passa o projeto ao formulário
+
+    return render(request, 'criar_analise.html', {'form': form, 'projeto': projeto})
+
+@login_required
+def editar_analise(request, projeto_id,id):
+    analise = get_object_or_404(Analise, id=id)
+
+    if request.method == 'POST':
+        form = AnaliseForm(request.POST, instance=analise)
+        if form.is_valid():
+            # Atribui o usuário logado ao campo 'alterado_por'
+            analise = form.save(commit=False)  # Não salva automaticamente
+            analise.alterado_por = request.user  # Atribui o usuário logado
+            analise.save()  # Salva o projeto com a alteração do usuário
+            return redirect('analise', id=projeto_id)  # Redireciona    s
+    else:
+        
+        form = AnaliseForm(instance=analise)
+        
+
+    return render(request, 'editar_analise.html', {'form': form, 'analise': analise})
+
+@login_required
+def excluir_analise(request, projeto_id, id):
+
+    analise = get_object_or_404(Analise, id=id)
+
+    # Exclui o projeto
+    analise.delete()
+
+    messages.success(request, "Projeto excluído com sucesso.")
+    return redirect('analise', id=projeto_id)  # Redireciona    
 
 @login_required
 def analise(request, id):
     amostras = Amostra.objects.all()
     return render(request, "amostra.html", {"amostras": amostras})
+
+@login_required
+def criar_amostra(request, analise_id):
+    analise = get_object_or_404(Analise, id=analise_id)  # Obtém a análise
+
+    if request.method == "POST":
+        form = AmostraForm(request.POST, analise=analise)  # Passa a análise ao formulário
+        if form.is_valid():
+            form.save(usuario=request.user)  # Passa o usuário logado
+            return redirect('detalhes_analise', analise_id=analise.id)  # Redireciona após salvar
+    else:
+        form = AmostraForm(analise=analise)  # Passa a análise ao formulário
+
+    return render(request, 'amostra_form.html', {'form': form, 'analise': analise})
+
+@login_required
+def editar_amostra(request, id):
+    projeto = get_object_or_404(Projeto, id=id)
+
+    if request.method == 'POST':
+        form = ProjetoForm(request.POST, instance=projeto)
+        if form.is_valid():
+            # Atribui o usuário logado ao campo 'alterado_por'
+            projeto = form.save(commit=False)  # Não salva automaticamente
+            projeto.alterado_por = request.user  # Atribui o usuário logado
+            projeto.save()  # Salva o projeto com a alteração do usuário
+            return redirect('index')  # Redireciona para a lista de projetos
+    else:
+        print(projeto.data)
+        form = ProjetoForm(instance=projeto)
+        form.fields['data'].initial = '2020-11-11'#projeto.data.strftime('%Y-%m-%d')
+        print(form.fields['data'].initial)
+
+    return render(request, 'editar_projeto.html', {'form': form, 'projeto': projeto})
+
+@login_required
+def excluir_amostra(request, id):
+    # Verifica se o usuário é administrador
+    if not request.user.administrador:
+        messages.error(request, "Você não tem permissão para excluir projetos.")
+        return redirect('index')  # Redireciona de volta para a página inicial ou outra página desejada
+
+    projeto = get_object_or_404(Projeto, id=id)
+
+    # Exclui o projeto
+    projeto.delete()
+
+    messages.success(request, "Projeto excluído com sucesso.")
+    return redirect('index')
